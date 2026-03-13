9ccea4d86a5b0eb8807bc2f781185bf3ed7eeb73
Jiajie Chen <c@jia.je>
2026-03-13 17:57:39 +0800

    Update mod image, add build.sh for packaging

--- build.sh
+++ build.sh
@@ -5,22 +5,12 @@ OS="$(uname -s)"
 
 case "$OS" in
     Linux*)
-        cp ~/.steam/steam/steamapps/common/Slay\ the\ Spire\ 2/data_sts2_linuxbsd_x86_64/sts2.dll .
-        ../Godot_v4.5.1-stable_mono_linux_x86_64/Godot_v4.5.1-stable_mono_linux.x86_64 --build-solutions --quit --headless --verbose
-        rm -rf dist
-        mkdir -p dist
-        cp ./.godot/mono/temp/bin/Debug/RouteSuggest.dll dist/
-        ../Godot_v4.5.1-stable_mono_linux_x86_64/Godot_v4.5.1-stable_mono_linux.x86_64 --export-pack "Windows Desktop" dist/RouteSuggest.pck --headless
-        cp RouteSuggest.json dist/RouteSuggest.json
+        STS_DLL="$HOME/.steam/steam/steamapps/common/Slay the Spire 2/data_sts2_linuxbsd_x86_64/sts2.dll"
+        GODOT="../Godot_v4.5.1-stable_mono_linux_x86_64/Godot_v4.5.1-stable_mono_linux.x86_64"
         ;;
     Darwin*)
-        cp ~/Library/Application\ Support/steam/steamapps/common/Slay\ the\ Spire\ 2/SlayTheSpire2.app/Contents/Resources/data_sts2_macos_arm64/sts2.dll .
-        /Applications/Godot_mono.app/Contents/MacOS/Godot --build-solutions --quit --headless --verbose
-        rm -rf dist
-        mkdir -p dist
-        cp ./.godot/mono/temp/bin/Debug/RouteSuggest.dll dist/
-        /Applications/Godot_mono.app/Contents/MacOS/Godot --export-pack "Windows Desktop" dist/RouteSuggest.pck --headless
-        cp RouteSuggest.json dist/RouteSuggest.json
+        STS_DLL="$HOME/Library/Application Support/steam/steamapps/common/Slay the Spire 2/SlayTheSpire2.app/Contents/Resources/data_sts2_macos_arm64/sts2.dll"
+        GODOT="/Applications/Godot_mono.app/Contents/MacOS/Godot"
         ;;
     *)
         echo "Unknown operating system: $OS"
@@ -28,3 +18,14 @@ case "$OS" in
         ;;
 esac
 
+cp "$STS_DLL" .
+$GODOT --build-solutions --quit --headless --verbose
+rm -rf dist
+mkdir -p dist
+cp ./.godot/mono/temp/bin/Debug/RouteSuggest.dll dist/
+$GODOT --export-pack "Windows Desktop" dist/RouteSuggest.pck --headless
+cp RouteSuggest.json dist/RouteSuggest.json
+
+VERSION=$(jq -r ".version" RouteSuggest.json)
+rm -f RouteSuggest-v$VERSION.zip
+cd dist && zip -r ../RouteSuggest-v$VERSION.zip .
